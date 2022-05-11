49e86f2c38be7fbc69c73801a31d516687e911fd
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-05-11 11:13:42 +0530

    styled components start homepage

--- src/components/jumbotron/jumbotron.component.jsx
+++ src/components/jumbotron/jumbotron.component.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import { Inner, Container } from "./styles/jumbotron.styles";
+import {
+  Item,
+  Inner,
+  Container,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from "./styles/jumbotron.styles";
 
 const Jumbotron = ({ children, direction = "row", ...otherProps }) => {
-  return <Inner direction={direction}>{children}</Inner>;
+  return (
+    <Item direction={direction}>
+      <Inner>{children}</Inner>
+    </Item>
+  );
 };
 
 Jumbotron.Container = function JumbotronContainer({ children, ...otherProps }) {
   return <Container {...otherProps}>{children}</Container>;
 }; // Compound components
 
+Jumbotron.Pane = function JumbotronPane({ children, ...otherProps }) {
+  return <Pane {...otherProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...otherProps }) {
+  return <Title {...otherProps}>{children}</Title>;
+};
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...otherProps }) {
+  return <SubTitle {...otherProps}>{children}</SubTitle>;
+};
+Jumbotron.Image = function JumbotronImage({ ...otherProps }) {
+  return <Image {...otherProps} />;
+};
 export default Jumbotron;
